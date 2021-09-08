35d3b1a6b8aba4228aff46fc5d1efe6cf42ea29c
Karl-Adrian <karladrian995@gmail.com>
2021-09-08 10:21:00 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+jQuery("h1").click(function () {
+  alert("This is a heading tag");
+});
+
+jQuery("p").click(function () {
+  alert("This is a paragraph");
+  alert("It talks about walruses");
+  alert("Walruses are cute");
+});
+
+jQuery("img").click(function () {
+  alert("WALRUS!!!!!!!!");
+  alert("pointy teeth!!!!!!!");
+  alert("OOOH THE CUTENESS");
+});
+
+jQuery("h1").click(function () {
+  alert("This is a header.");
+  alert("I told you, THIS IS A HEADER!!");
+});
+
+jQuery("h2").click(function () {
+  alert("why you clicking me??");
+  alert("stop it!!");
+  alert("leave me alone");
+});
